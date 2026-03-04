c9a3752f658b7a2457ddd2652a840ae80906c961
zhongjiehao <zhongjiehao@126.com>
2026-03-04 10:14:59 +0800

    add a Solution: detect-person

--- easyeai-api/media/rga/rga_wrapper.h
+++ easyeai-api/media/rga/rga_wrapper.h
@@ -22,20 +22,20 @@
 extern "C"{
 #endif
 
-
+/* rotate source image 0 degrees clockwise */
+#define HAL_TRANSFORM_ROT_0      0x00
+#if 0 // ---此组定义已写在<rga/RgaApi.h>中
 /* flip source image horizontally (around the vertical axis) */
 #define HAL_TRANSFORM_FLIP_H     0x01
 /* flip source image vertically (around the horizontal axis)*/
 #define HAL_TRANSFORM_FLIP_V     0x02
-/* rotate source image 0 degrees clockwise */
-#define HAL_TRANSFORM_ROT_0      0x00
-/* rotate source image 90 degrees clockwise */
-#define HAL_TRANSFORM_ROT_90     0x04
 /* rotate source image 180 degrees */
 #define HAL_TRANSFORM_ROT_180    0x03
+/* rotate source image 90 degrees clockwise */
+#define HAL_TRANSFORM_ROT_90     0x04
 /* rotate source image 270 degrees clockwise */
 #define HAL_TRANSFORM_ROT_270    0x07
-
+#endif
 
 typedef struct {
     RgaSURF_FORMAT fmt;
@@ -47,7 +47,7 @@ typedef struct {
     void *pBuf;
 }Image;
 
-
+//*strFmt: "NV12"、"NV21"、"RGB"、"BGR"
 extern RgaSURF_FORMAT rgaFmt(char *strFmt);
 
 extern void rga_init();
